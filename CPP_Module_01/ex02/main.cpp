4bdcb7a0c3b70b1a3d218092f8e6295ba280ca36
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-02-24 19:41:05 +0300

    ex02 v1.1

--- CPP_Module_01/ex02/main.cpp
+++ CPP_Module_01/ex02/main.cpp
@@ -33,5 +33,18 @@ int		main()
 	std::cout << "\x1b[31;1mStack Zombie\x1b[0m" << std::endl;
 	Zombie	zombie = Zombie("Alex", "Human");
 	zombie.announce();
+
+	std::cout << "\x1b[95;1m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\x1b[0m" << std::endl;
+	std::cout << "\x1b[31;1mYour name and type!\x1b[0m" << std::endl;
+	std::string	name;
+	std::string	type;
+	std::cout << "\x1b[32;1mGive me name > \x1b[0m";
+	std::getline(std::cin, name);
+	std::cout << "\x1b[33;1mGive me type > \x1b[0m";
+	std::getline(std::cin, type);
+	zombieevent.setZombieType(type);
+	zombies = zombieevent.newZombie(name);
+	zombies->announce();
+	delete zombies;
 	return 0;
 }
