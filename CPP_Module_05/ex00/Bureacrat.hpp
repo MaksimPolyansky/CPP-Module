4c1f02433e24929ae0e1ee1c5ad997befe5b9d13
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-03-23 22:51:43 +0300

    05 ex00 v1.0

--- CPP_Module_05/ex00/Bureacrat.hpp
+++ CPP_Module_05/ex00/Bureacrat.hpp
@@ -16,8 +16,23 @@ class Bureacrat
 
 		Bureacrat			&operator=(Bureacrat const &bu);
 
-		void				GradeTooHighException();
-		void				GradeTooLowException();
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade Too High Exception");
+				}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade Too Low Exception");
+				}
+		};
 
 		std::string			getName() const;
 		int					getGrade() const;
@@ -27,66 +42,5 @@ class Bureacrat
 };
 
 std::ostream				&operator<<(std::ostream &output, Bureacrat const &bu);
-
-Bureacrat::Bureacrat(std::string name, int grade) : _name(name)
-{
-	if (grade < 1)
-		Bureacrat::GradeTooLowException();
-	if (grade > 150)
-		Bureacrat::GradeTooHighException();
-	this->_grade = grade;
-}
-
-Bureacrat::Bureacrat(Bureacrat const &copy) : _name(copy._name)
-{
-	this->_grade = copy._grade;
-}
-
-Bureacrat::~Bureacrat()
-{
-}
-
-Bureacrat			&Bureacrat::operator=(Bureacrat const &bu)
-{
-	if (this != &bu)
-		this->_grade = bu._grade;
-	return *this;
-}
-
-void				Bureacrat::GradeTooHighException()
-{}
-
-void				Bureacrat::GradeTooLowException()
-{}
-
-std::string			Bureacrat::getName() const
-{
-	return this->_name;
-}
-
-int					Bureacrat::getGrade() const
-{
-	return this->_grade;
-}
-
-void				Bureacrat::gradeMinus()
-{
-	this->_grade++;
-	if (this->_grade > 150)
-		Bureacrat::GradeTooHighException();
-}
-
-void				Bureacrat::gradePlus()
-{
-	this->_grade--;
-	if (this->_grade < 1)
-		Bureacrat::GradeTooLowException();
-}
-
-std::ostream		&operator<<(std::ostream &output, Bureacrat const &bu)
-{
-	output << "\e[1;36m<" << bu.getName() << ">, bureaucrat grade <" << bu.getGrade() << ">";
-	return output;
-}
 
 #endif
