89a47670353a7fb6ae25d593263937c0807e636c
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-03-24 22:56:27 +0300

    05 ex01 v1.1

--- CPP_Module_05/ex01/Form.hpp
+++ CPP_Module_05/ex01/Form.hpp
@@ -50,6 +50,10 @@ std::ostream				&operator<<(std::ostream &output, Form const &fo);
 Form::Form(std::string const &name, int const grade_sign, int const grade_execute)
 	: _name(name), _grade_sign(grade_sign), _grade_execute(grade_execute)
 {
+	if (grade_sign < 1 || grade_execute < 1)
+		throw Form::GradeTooHighException();
+	if (grade_sign > 150 || grade_execute > 150)
+		throw Form::GradeTooLowException();
 	this->_boolean = false;
 }
 
@@ -57,6 +61,10 @@ Form::Form(Form const &copy)
 	: _name(copy._name), _grade_sign(copy._grade_sign),
 	_grade_execute(copy._grade_execute)
 {
+	if (copy._grade_sign < 1 || copy._grade_execute < 1)
+		throw Form::GradeTooHighException();
+	if (copy._grade_sign > 150 || copy._grade_execute > 150)
+		throw Form::GradeTooLowException();
 	this->_boolean = copy._boolean;
 }
 
@@ -67,25 +75,42 @@ Form::~Form()
 Form				&Form::operator=(Form const &form)
 {
 	if (this != &form)
-		this->_boolean = copy._boolean;
+		*this = form;
 	return *this;
 }
 
 std::string			Form::getName() const
-{}
+{
+	return this->_name;
+}
 
 bool				Form::getBoolean() const
-{}
+{
+	return this->_boolean;
+}
 
 int					Form::getGradeSign() const
-{}
+{
+	return this->_grade_sign;
+}
 
 int					Form::getGradeExecute() const
-{}
+{
+	return this->_grade_execute;
+}
+/*
+void				Form::beSign(Bureacrat const &bu)
+{
 
+}
+*/
 std::ostream				&operator<<(std::ostream &output, Form const &fo)
 {
-	output << "\e[1;36m<" << fo.getName() << "> has grade <" << fo.getGrade() << ">\e[0m" << std::endl;
+	output << "\e[1;36m" << fo.getName();
+	output << ": will sign only at a grade of ";
+	output << fo.getGradeSign();
+	output << ", and will be executed only at a grade of ";
+	output << fo.getGradeSign() << "!>\e[0m" << std::endl;
 	return output;
 }
 
