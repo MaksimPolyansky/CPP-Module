93acba4e6cda6282b00b9657d29aa6abd117e102
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-03-28 10:02:44 +0300

    04 ex02 v1.2

--- CPP_Module_04/ex02/main.cpp
+++ CPP_Module_04/ex02/main.cpp
@@ -19,5 +19,22 @@ int main()
 	}
 	delete vlc;
 
+	//Squad *ninja = new Squad();
+	//ninja->push(jim);
+	//std::cout << ninja->getCount() << std::endl;
+	//ninja->getUnit(0);
+	//Squad *li = new Squad(*ninja);
+	//std::cout << li->getCount() << std::endl;
+	//li->getUnit(0);
+
+	//ISpaceMarine *first = new TacticalMarine;
+	//first->battleCry();
+	//std::cout << first << std::endl;
+
+	//ISquad *second = new Squad;
+	//second->push(first);
+	//second->push(first);
+	//std::cout << second->getCount() << " = count" << std::endl;
+
 	return 0;
 }
