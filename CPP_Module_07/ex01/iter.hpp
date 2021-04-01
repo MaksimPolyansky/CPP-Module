681ebebc901f7d4030794652b3f59f06e4e1f7b8
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-04-01 21:08:52 +0300

    07 ex01 v1.1

--- CPP_Module_07/ex01/iter.hpp
+++ CPP_Module_07/ex01/iter.hpp
@@ -3,4 +3,27 @@
 
 #include <iostream>
 
+template <typename T>
+void	iter(T *array, size_t array_lenght, void (*func)(T const &))
+{
+	for (size_t i = 0; i < array_lenght; i++)
+	{
+		func(array[i]);
+	}
+}
+
+// class Awesome
+// {
+// 	public:
+// 		Awesome(void) : _n(42) { return; }
+// 		int		get(void) const { return this->_n; }
+// 	private:
+// 		int		_n;
+// };
+
+// std::ostream	&operator<<(std::ostream &o, Awesome const &rhs) { o << rhs.get(); return o; }
+
+// template<typename T>
+// void print( T const & x ) { std::cout << x << std::endl; return; }
+
 #endif
--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -1,6 +1,27 @@
 #include "iter.hpp"
 
+void	print_string(std::string const &str)
+{
+	std::cout << "\e[1;36m<" << str << ">\e[0m" << std::endl;
+}
+
+void	print_sum(double num)
+{
+	std::cout << num + 42.2 << std::endl;
+}
+
 int		main(void)
 {
+	std::cout << std::endl << "\e[1;92m___FIRST TEST___\e[0m" << std::endl;
+	std::string	line[5] = {"first", "second", "third", "fourth", "fifth"};
+	iter(line, 5, print_string);
+	std::cout << std::endl;
 
+	// std::cout << std::endl << "\e[1;92m___CHECKLIST TEST___\e[0m" << std::endl;
+	// int	tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+	// Awesome tab2[5];
+	// iter(tab, 5, print);
+	// iter(tab2, 5, print);
+
+	return 0;
 }
