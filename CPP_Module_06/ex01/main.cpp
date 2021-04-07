ae7576d0ab6e04368f666e3e01603f80da31737a
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-04-08 00:01:22 +0300

    06 ex01 v1.1

--- CPP_Module_06/ex01/main.cpp
+++ CPP_Module_06/ex01/main.cpp
@@ -1,38 +1,65 @@
 #include <iostream>
 #include <ctime>
 
-typedef struct	Data
+struct	Data
 {
-    std::string	line1;
-    int			num;
-    std::string	line2;
+    char	*line1;
+    int		num;
+    char	*line2;
 };
 
 void			*serialize(void)
 {
-	char		*line = new char[sizeof(std::string) * 2 + sizeof(int)];
+	char		*line = new char[sizeof(char*) * 2 + sizeof(int)];
 
-	size_t		i = 0;
-	for (; i < sizeof(std::string); i++)
+	for (size_t k = 0; k < (sizeof(char*) * 2 + sizeof(int)); k++)
 	{
-		*reinterpret_cast<std::string*>(line + i) = 97 + std::rand() % 26;
+		line[k] = 0;
 	}
-	*reinterpret_cast<int*>(line + i++) = std::rand();
-	for (; i < sizeof(std::string); i++)
+	char		line1[16];
+	for (size_t i = 0; i < 16; i++)
 	{
-		*reinterpret_cast<std::string*>(line + i) = 97 + std::rand() % 26;
+		line1[i] = (char)(97 + std::rand() % 26);
 	}
-	return reinterpret_cast<void*>(line);
+	std::cout << line1 << std::endl;
+	*reinterpret_cast<char**>(line) = line1;
+	std::cout << *reinterpret_cast<char**>(line) << std::endl;
+	*reinterpret_cast<int*>(line + sizeof(char*)) = std::rand();
+	std::cout << *reinterpret_cast<int*>(line + sizeof(char*)) << std::endl;
+	char		line2[16];
+	for (size_t i = 0; i < 16; i++)
+	{
+		line2[i] = 97 + std::rand() % 26;
+	}
+	*reinterpret_cast<char**>(line + sizeof(char*) + sizeof(int)) = line2;
+	std::cout << *reinterpret_cast<char**>(line + sizeof(char*) + sizeof(int)) << std::endl;
+	return static_cast<void*>(line);
 }
 
 Data			*deserialize(void *raw)
 {
 	Data		*data = new Data;
+
+	data->line1 = *reinterpret_cast<char**>(raw);
+	data->num = *reinterpret_cast<int*>(static_cast<char*>(raw) + sizeof(char*));
+	data->line2 = *reinterpret_cast<char**>(static_cast<char*>(raw) + sizeof(char*) + sizeof(int));
+
+	return data;
 }
 
 int				main()
 {
 	std::srand(std::time(NULL));
+
+	void	*pointer = serialize();
+	Data	*data = deserialize(pointer);
+
+	std::cout << data->line1 << std::endl;
+	std::cout << data->num << std::endl;
+	std::cout << data->line2 << std::endl;
+
+	delete	(char*)pointer;
+	delete	data;
 
     return 0;
 }
