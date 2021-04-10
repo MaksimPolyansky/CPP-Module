1fac743f3da09713ed58f79e0aa889451b3dd49f
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-04-10 23:19:10 +0300

    08 ex00 v1.1

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -2,9 +2,21 @@
 # define EASYFIND_HPP
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <array>
 
 template <typename T>
-T   easyfind()
-{}
+typename T::iterator	easyfind(T &container, int find)
+{
+	if (container.empty())
+		throw std::string("Container is empty!");
+	typename T::iterator	i;
+	i = std::find(container.begin(), container.end(), find);
+	if (i == container.end())
+		throw std::string("Container has not element!");
+	return i;
+}
 
 #endif
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -1,4 +1,17 @@
 #include "easyfind.hpp"
 
 int		main()
-{}
+{
+	std::cout << "\e[1;37mFIRST!\e[0m" << std::endl;
+	try
+	{
+		std::vector<int>	myVector(21, 42);
+	}
+	catch(const std::string& e)
+	{
+		std::cerr << e << '\n';
+	}
+	return 0;
+}
+
+// clang++ -Wall -Werror -Wextra -std=c++98 *.cpp
