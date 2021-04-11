7a0e2e7ce8e50d29a34881a79101423130675299
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-04-11 17:33:53 +0300

    08 ex02 v1.0

--- CPP_Module_08/ex02/mutantstack.cpp
+++ CPP_Module_08/ex02/mutantstack.cpp
@@ -1,1 +1,2 @@
 #include "mutantstack.hpp"
+
--- CPP_Module_08/ex02/mutantstack.hpp
+++ CPP_Module_08/ex02/mutantstack.hpp
@@ -1,6 +1,21 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <stack>
 
+template <typename T>
+class Mutantstack : public std::stack<T>
+{
+	public:
+		Mutantstack(/* args */) : std::stack<int>(){};
+		Mutantstack(Mutantstack const &copy) : std::stack<int>(copy){};
+		virtual ~Mutantstack() {};
+
+		Mutantstack		&operator=(Mutantstack const &mu) {this->c = mu.c; return *this;};
+};
 
 #endif
